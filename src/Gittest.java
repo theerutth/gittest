d35c5a5aa1ad1283d7ecedc777fc64d7218dbf62
theerut <natnicha.987231@mail.kmutt.ac.th>
2020-10-20 15:13:25 +0700

    First Commit

---
+++ src/Gittest.java
@@ -0,0 +1,12 @@
+
+public class Gittest {
+
+    public static void main(String[] args) {
+        sayhello();
+    }
+
+    public static void sayhello() {
+        System.out.println("Hello");
+    }
+
+}
